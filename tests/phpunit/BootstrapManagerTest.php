afe65d9411d2d28f070b6d63fb0710ab84d6d08b
Pierre Boutet <boutetpierre@yahoo.fr>
2018-12-18 15:15:13 +0100

    improve WfTutorialsUtils

--- tests/phpunit/BootstrapManagerTest.php
+++ tests/phpunit/BootstrapManagerTest.php
@@ -53,14 +53,57 @@ class WfTutorialUtilsTest extends \PHPUnit_Framework_TestCase {
 	}
 
 
+	public function testSimple( ) {
+
+
+		$content = "{{ {{tntn|Tuto Details}}
+		|Type=Technique
+		|Main_Picture=toto.jpg
+		|Area=Clothing and Accessories
+		|Tags=test,
+		|Description=test de tuto en francais
+		|Difficulty=Very easy
+		|Cost=2
+		|Currency=EUR (€)
+		|Duration=3
+		|Duration-type=minute(s)
+		|Licences=Attribution (CC BY)
+	}}
+	{{ {{tntn|Introduction}}}}
+	{{ {{tntn|Materials}}}}
+	{{ {{tntn|Separator}}}}
+	{{ {{tntn|Tuto Step}}
+	|Step_Title=debrouille toi vite
+	}}
+	{{ {{tntn|Notes}}}}
+	{{ {{tntn|Tuto Status}}}}";
+		$result = $this->instance->getArticleData($content);
+
+		$expected = [
+			'Type' => 'Technique',
+			'Main_Picture' => 'toto.jpg',
+			'Area' => 'Clothing and Accessories',
+			'Tags' => 'test,',
+			'Description' => 'test de tuto en francais',
+			'Difficulty' => 'Very easy',
+			'Cost' => '2',
+			'Currency' => 'EUR (€)',
+			'Duration' => '3',
+			'Duration-type' => 'minute(s)',
+			'Licences' => 'Attribution (CC BY)',
+			'Step_Title' => 'debrouille toi vite'
+		];
+
+		$this->assertEquals($expected,$result);
+	}
 	public function testJsonData( ) {
 
 		$jsondata = "{\"version\":\"2.4.1\",\"objects\":[{\"type\":\"image\",\"version\":\"2.4.1\",\"originX\":\"left\",\"originY\":\"top\",\"left\":-342.65,\"top\":-146.37,\"width\":800,\"height\":450,\"fill\":\"rgb(0,0,0)\",\"stroke\":null,\"strokeWidth\":0,\"strokeDashArray\":null,\"strokeLineCap\":\"butt\",\"strokeLineJoin\":\"miter\",\"strokeMiterLimit\":4,\"scaleX\":2.49,\"scaleY\":2.49,\"angle\":0,\"flipX\":false,\"flipY\":false,\"opacity\":1,\"shadow\":null,\"visible\":true,\"clipTo\":null,\"backgroundColor\":\"\",\"fillRule\":\"nonzero\",\"paintFirst\":\"fill\",\"globalCompositeOperation\":\"source-over\",\"transformMatrix\":null,\"skewX\":0,\"skewY\":0,\"crossOrigin\":\"\",\"cropX\":0,\"cropY\":0,\"src\":\"http://demo-dokit.localtest.me/w/images/thumb/f/f7/Tuto_test_images_LB_Step_44.jpg/800px-Tuto_test_images_LB_Step_44.jpg\",\"filters\":[]},{\"type\":\"wfcircle\",\"version\":\"2.4.1\",\"originX\":\"center\",\"originY\":\"center\",\"left\":367.29,\"top\":253.29,\"width\":200,\"height\":200,\"fill\":\"rgba(255,0,0,0)\",\"stroke\":\"red\",\"strokeWidth\":2.36,\"strokeDashArray\":null,\"strokeLineCap\":\"butt\",\"strokeLineJoin\":\"miter\",\"strokeMiterLimit\":4,\"scaleX\":1.27,\"scaleY\":1.27,\"angle\":0,\"flipX\":false,\"flipY\":false,\"opacity\":1,\"shadow\":null,\"visible\":true,\"clipTo\":null,\"backgroundColor\":\"\",\"fillRule\":\"nonzero\",\"paintFirst\":\"fill\",\"globalCompositeOperation\":\"source-over\",\"transformMatrix\":null,\"skewX\":0,\"skewY\":0,\"radius\":100,\"startAngle\":0,\"endAngle\":6.283185307179586}],\"height\":450,\"width\":600}";
 
 		$content = "{{ {{tntn|Tuto Details}}
 |Type=Technique
-|Main_Picutre=toto.jpg
-|Main_Picutre_annotation=$jsondata
+|Main_Picture=toto.jpg
+|Main_Picture_annotation=$jsondata
 |Area=Clothing and Accessories
 |Tags=test,
 |Description=test de tuto en francais
@@ -81,7 +124,37 @@ class WfTutorialUtilsTest extends \PHPUnit_Framework_TestCase {
 {{ {{tntn|Tuto Status}}}}";
 		$result = $this->instance->getArticleData($content);
 
-		$this->assertEquals($result['Main_Picutre_annotation'], $jsondata);
+		$this->assertEquals( $jsondata, $result['Main_Picture_annotation']);
+	}
+	public function testJsonDataEnd( ) {
+
+		$jsondata = "{\"version\":\"2.4.1\",\"objects\":[{\"type\":\"image\",\"version\":\"2.4.1\",\"originX\":\"left\",\"originY\":\"top\",\"left\":-342.65,\"top\":-146.37,\"width\":800,\"height\":450,\"fill\":\"rgb(0,0,0)\",\"stroke\":null,\"strokeWidth\":0,\"strokeDashArray\":null,\"strokeLineCap\":\"butt\",\"strokeLineJoin\":\"miter\",\"strokeMiterLimit\":4,\"scaleX\":2.49,\"scaleY\":2.49,\"angle\":0,\"flipX\":false,\"flipY\":false,\"opacity\":1,\"shadow\":null,\"visible\":true,\"clipTo\":null,\"backgroundColor\":\"\",\"fillRule\":\"nonzero\",\"paintFirst\":\"fill\",\"globalCompositeOperation\":\"source-over\",\"transformMatrix\":null,\"skewX\":0,\"skewY\":0,\"crossOrigin\":\"\",\"cropX\":0,\"cropY\":0,\"src\":\"http://demo-dokit.localtest.me/w/images/thumb/f/f7/Tuto_test_images_LB_Step_44.jpg/800px-Tuto_test_images_LB_Step_44.jpg\",\"filters\":[]},{\"type\":\"wfcircle\",\"version\":\"2.4.1\",\"originX\":\"center\",\"originY\":\"center\",\"left\":367.29,\"top\":253.29,\"width\":200,\"height\":200,\"fill\":\"rgba(255,0,0,0)\",\"stroke\":\"red\",\"strokeWidth\":2.36,\"strokeDashArray\":null,\"strokeLineCap\":\"butt\",\"strokeLineJoin\":\"miter\",\"strokeMiterLimit\":4,\"scaleX\":1.27,\"scaleY\":1.27,\"angle\":0,\"flipX\":false,\"flipY\":false,\"opacity\":1,\"shadow\":null,\"visible\":true,\"clipTo\":null,\"backgroundColor\":\"\",\"fillRule\":\"nonzero\",\"paintFirst\":\"fill\",\"globalCompositeOperation\":\"source-over\",\"transformMatrix\":null,\"skewX\":0,\"skewY\":0,\"radius\":100,\"startAngle\":0,\"endAngle\":6.283185307179586}],\"height\":450,\"width\":600}";
+
+		$content = "{{ {{tntn|Tuto Details}}
+|Type=Technique
+|Main_Picture=toto.jpg
+|Area=Clothing and Accessories
+|Tags=test,
+|Description=test de tuto en francais
+|Difficulty=Very easy
+|Cost=2
+|Currency=EUR (€)
+|Duration=3
+|Duration-type=minute(s)
+|Licences=Attribution (CC BY)
+|Main_Picture_annotation=$jsondata
+}}
+{{ {{tntn|Introduction}}}}
+{{ {{tntn|Materials}}}}
+{{ {{tntn|Separator}}}}
+{{ {{tntn|Tuto Step}}
+|Step_Title=debrouille toi vite
+}}
+{{ {{tntn|Notes}}}}
+{{ {{tntn|Tuto Status}}}}";
+		$result = $this->instance->getArticleData($content);
+
+		$this->assertEquals($jsondata,$result['Main_Picture_annotation']);
 	}
 
 }
